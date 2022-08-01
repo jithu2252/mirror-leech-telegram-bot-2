c9241de1c78204a97fc10711d7e90e2d8a6cea8f
Romeo <92579700+JohnWickKeanue@users.noreply.github.com>
2022-08-01 19:05:52 +0530

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -15,7 +15,7 @@ from json import loads as jsnloads
 from lk21 import Bypass
 from cfscrape import create_scraper
 from bs4 import BeautifulSoup
-from base64 import standard_b64encode
+from base64 import b64decode, standard_b64encode
 
 from bot import LOGGER, UPTOBOX_TOKEN
 from bot.helper.telegram_helper.bot_commands import BotCommands
